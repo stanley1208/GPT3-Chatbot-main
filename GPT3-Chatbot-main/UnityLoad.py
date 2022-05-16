293ae16ca41ee0e45d98611a469e88506c12d091
stanley <stanley.mei08@gmail.com>
2022-05-16 14:02:33 +0800

    update

---
+++ GPT3-Chatbot-main/UnityLoad.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+PoemName = "杜甫《江畔獨步尋花·其六》"
+PoemTest = """
+黃四孃家花滿蹊，千朵萬朵壓枝低。
+留連戲蝶時時舞，自在嬌鶯恰恰啼。
+"""
+
+print(PoemName)
+print(PoemTest)
+
+MottoEn = "A warm smile is the universal language of kindness."
+MottoCh = "溫暖的微笑是表示善意的通用語言。"
